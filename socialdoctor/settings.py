6c80bd7986640c50bfcffb7e98b64480d95304cc
Nikita Lyubchich <cybran111@gmail.com>
2015-05-13 18:13:49 +0300

    Work on Postgres

--- socialdoctor/settings.py
+++ socialdoctor/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,13 +76,17 @@ WSGI_APPLICATION = 'socialdoctor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
+DATABASES = dict()
+
+# Production/debug databases
+if "DJANGO_PROD" in os.environ:
+    DATABASES['default'] = dj_database_url.config()
+    # Enable Connection Pooling
+    DATABASES['default']['ENGINE'] = 'django_postgrespool'
+
+else:
+    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
