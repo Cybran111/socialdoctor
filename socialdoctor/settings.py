acb67bce55cdb6336ab3e38c524fc3d1e2144392
Nikita Lyubchich <cybran111@gmail.com>
2015-05-13 18:33:28 +0300

    Fixing static

--- socialdoctor/settings.py
+++ socialdoctor/settings.py
@@ -107,10 +107,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'social', 'static'),
 )
