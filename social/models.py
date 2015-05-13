b4f6ca2cf71bb080c538bacbc675ff8be4a29821
Nikita Lyubchich <cybran111@gmail.com>
2015-05-13 12:55:30 +0300

    Added new model Added dummy base.html Added DB to gitignore

--- social/models.py
+++ social/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    TYPE_CHOICES = (
+        ("doctor", 'Doctor'),
+        ('patient', 'Patient'),
+    )
+
+    user = models.OneToOneField(get_user_model())
+    followers = models.ManyToManyField(get_user_model())
+    user_type = models.TextField(choices=TYPE_CHOICES)
